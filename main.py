b23c10a84e73a9f8ac2deb84b232dd5c70e0ef0e
Angus Lowe <anguslowe@live.ca>
2017-01-24 22:54:33 +0000

    imported all of sympy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import json
-from sympy import latex
+from sympy import *
 
 app = Flask(__name__)
 letters = string.lowercase[:26]
